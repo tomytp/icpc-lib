b506ed7cf2b02f53ce17060d8b6a8c0128670c26
tomytp <thomazrebellomiranda@gmail.com>
2025-10-12 08:48:39 -0300

    template

--- src/extra/template.cpp
+++ src/extra/template.cpp
@@ -6,14 +6,18 @@ typedef pair<ll, ll> p64;
 typedef vector<ll> v64;
 
 #define forn(i, s, e) for(ll i = (s); i < (e); i++)
+#define sz(x) ((ll) x.size())
 #define ln "\n"
 
-#if defined(DEBUG)
-    #define _ (void)0
-    #define debug(x) cout << __LINE__ << ": " << #x << " = " << x << ln
-#else
-    #define _ ios_base::sync_with_stdio(false), cin.tie(NULL)
-    #define debug(x) (void)0
+#define trace(x) (void)0
+#define _ ios_base::sync_with_stdio(false), cin.tie(NULL)
+#define watchr(a, b) trace(for (auto c=a; c!=b;) cout << *(c++) << " "; cout << ln;)
+#define watchv(v) trace({cout << #v": "; WATCHR(v.begin(), v.end());})
+#define debug(x) trace(cout << __LINE__ << ": " #x " = " << x << ln)
+
+#ifdef DEBUG
+#define _ (void)0
+    #define trace(x) x
 #endif
 
 const ll INF = 0x3f3f3f3f3f3f3f3fll;
