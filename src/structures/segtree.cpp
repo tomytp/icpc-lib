a0a17062cee3e2bf072a3de63d7cae837ed723d6
tomytp <thomazrebellomiranda@gmail.com>
2025-10-12 09:30:24 -0300

    segtree

--- src/structures/segtree.cpp
+++ src/structures/segtree.cpp
@@ -12,42 +12,37 @@ struct node {
     }
 };
 
-const node neutral = {0};
+template<typename T> struct segtree {
+  ll n;
+  T neutral;
+  vector<T> tree;
 
-struct tree {
-    ll lm, rm;
-    unique_ptr<tree> lc, rc;
-    
-    node val;
+  segtree<T>(ll _n, T _neutral = T()) {
+    n = _n, neutral = _neutral;
+    tree.resize(2*n+1, neutral);
+  }
 
-    tree(ll l_, ll r_, const vector<node>& v) : lm(l_), rm(r_) {
-        if (lm == rm) {
-            val = v[lm];
-        } else {
-            ll m = (lm + rm) / 2;
-            lc = make_unique<tree>(lm, m, v);
-            rc = make_unique<tree>(m + 1, rm, v);
-            pull();
-        }
+  void set_leaves(vector<T> &leaves) {
+    copy(leaves.begin(), leaves.end(), tree.begin() + n);
+
+    for (ll i = n - 1; i > 0; i--) tree[i] = T::comb(tree[2 * i], tree[2 * i + 1]);
+  }
+
+  void update(ll i, T v) {
+    i += n;
+    tree[i] = v;
+    while (i > 1) {
+      i /= 2;
+      tree[i] = T::comb(tree[2 * i], tree[2 * i + 1]);
     }
+  }
 
-    void pull() {
-        val = node::comb(lc->val, rc->val);
+  T query(ll i, ll j) {
+    T rl = neutral, rr = neutral;
+    for(i += n, j += n; i <= j; i /= 2, j /= 2){
+      if((i&1) == 1) rl = T::comb(rl, tree[i++]);
+      if((j&1) == 0) rr = T::comb(tree[j--], rr);
     }
-
-    void point_set(ll idx, node x) {
-        if (lm == rm) {
-            val = x;
-            return;
-        }
-        if (idx <= lc->rm) lc->point_set(idx, x);
-        else rc->point_set(idx, x);
-        pull();
-    }
-
-    node query(ll lq, ll rq) {
-        if (rq < lm || lq > rm) return neutral;
-        if (lq <= lm && rm <= rq) return val;
-        return node::comb(lc->query(lq, rq), rc->query(lq, rq));
-    }
+    return T::comb(rl, rr);
+  }
 };
