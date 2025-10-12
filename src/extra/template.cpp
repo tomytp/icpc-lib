cbd6a45421d60135d24f21ad653673f0d4812ad1
tomytp <thomazrebellomiranda@gmail.com>
2025-10-12 09:04:17 -0300

    template

--- src/extra/template.cpp
+++ src/extra/template.cpp
@@ -9,16 +9,16 @@ typedef vector<ll> v64;
 #define sz(x) ((ll) x.size())
 #define ln "\n"
 
-#define trace(x) (void)0
-#define _ ios_base::sync_with_stdio(false), cin.tie(NULL)
-#define watchr(a, b) trace(for (auto c=a; c!=b;) cout << *(c++) << " "; cout << ln;)
-#define watchv(v) trace({cout << #v": "; WATCHR(v.begin(), v.end());})
-#define debug(x) trace(cout << __LINE__ << ": " #x " = " << x << ln)
-
 #ifdef DEBUG
-#define _ (void)0
     #define trace(x) x
+    #define _ (void)0
+#else
+    #define trace(x) (void)0
+    #define _ ios_base::sync_with_stdio(false), cin.tie(NULL)
 #endif
+
+#define debugv(v) trace({cout << #v": "; for (auto x : v) cout<< x << " "; cout << ln;})
+#define debug(x) trace(cout << __LINE__ << ": " #x " = " << x << ln)
 
 const ll INF = 0x3f3f3f3f3f3f3f3fll;
 
