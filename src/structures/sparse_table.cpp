e822db894acfba164c872ea952195d5844561125
MiguelABatista <miguel260503@gmail.com>
2025-11-03 14:22:08 -0300

    Change sparse to a struct

--- src/structures/sparse_table.cpp
+++ src/structures/sparse_table.cpp
@@ -4,23 +4,23 @@
 //
 // complexity: O(N log N) build, O(1) query; O(N log N) space
 
-ll m[MAXN][MAXLOGN];
-
-void build(v64& v) {
-    ll sz = v.size();
+struct sparse {
+    vector<v64> m;
     
-    forn(i, 0, sz) {
-        m[i][0] = v[i];
+    sparse(v64& v) {
+        ll n = sz(v);
+        ll logn = 64 - __builtin_clzll(n - 1);
+        m.resize(n+1, v64(logn+1));
+
+        forn(i, 0, n) m[i][0] = v[i];
+
+        for (ll j = 1; (1 << j) <= n; j++)
+        for (ll i = 0; i + (1 << j) <= n; i++)
+            m[i][j] = min(m[i][j-1], m[i + (1 << (j-1))][j-1]);    
     }
     
-    for (ll j = 1; (1 << j) <= sz; j++) {
-        for (ll i = 0; i + (1 << j) <= sz; i++) {
-            m[i][j] = max(m[i][j-1], m[i + (1 << (j-1))][j-1]);
-        }
+    ll query(ll a, ll b) { 
+        ll j = __builtin_clzll(1) - __builtin_clzll(b - a + 1);
+        return min(m[a][j], m[b - (1 << j) + 1][j]);
     }
-}
-
-ll query(ll a, ll b) {
-    ll j = __builtin_clzll(1) - __builtin_clzll(b - a + 1);
-    return max(m[a][j], m[b - (1 << j) + 1][j]);
-}
+};
