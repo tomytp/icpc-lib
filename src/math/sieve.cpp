07db30bcdb99cc662baa70801debd12f3ee703c5
MiguelABatista <miguel260503@gmail.com>
2025-11-03 14:21:58 -0300

    Fix sieve

--- src/math/sieve.cpp
+++ src/math/sieve.cpp
@@ -10,15 +10,17 @@
 const ll N = 10000000;
 v64 lp(N+1), pr;
 
-forn(i, 2, N+1) {
-    if (lp[i] == 0) {
-        lp[i] = i;
-        pr.push_back(i);
-    }
-    for (ll j = 0; i * pr[j] <= N; ++j) {
-        lp[i * pr[j]] = pr[j];
-        if (pr[j] == lp[i]) {
-            break;
+void sieve(){
+    forn(i, 2, N+1) {
+        if (lp[i] == 0) {
+            lp[i] = i;
+            pr.push_back(i);
+        }
+        for (ll j = 0; i * pr[j] <= N; ++j) {
+            lp[i * pr[j]] = pr[j];
+            if (pr[j] == lp[i]) {
+                break;
+            }
         }
     }
 }
