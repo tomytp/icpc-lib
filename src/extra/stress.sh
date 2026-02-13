dec5507a24e0a3297e4b031bf1c9db73884214d0
MiguelABatista <miguel260503@gmail.com>
2026-02-13 13:51:36 -0300

    Fix typo on stress.sh

--- src/extra/stress.sh
+++ src/extra/stress.sh
@@ -2,7 +2,7 @@ P=a
 make ${P} ${P}2 gen || exit 1
 for ((i = 1; ; i++)) do
 	./gen $i > tin
-	./${P} < tin > tout_
+	./${P} < tin > tout
 	./${P}2 < tin > tout2
 	if (! cmp -s tout tout2) then
 		echo "--> entrada:"
