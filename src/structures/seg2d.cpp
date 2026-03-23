7bfaa7d45358b34ad56983dfcc639b59d3e52b42
MiguelABatista <miguel260503@gmail.com>
2026-03-23 17:11:29 -0300

    Add seg2d

---
+++ src/structures/seg2d.cpp
@@ -0,0 +1,56 @@
+// SegTree 2D Iterativa
+//
+// Consultas 0-based
+// Um valor inicial em (x, y) deve ser colocado em seg[x+n][y+n]
+// Query: soma do retangulo ((x1, y1), (x2, y2))
+// Update: muda o valor da posicao (x, y) para val
+// Nao pergunte como que essa coisa funciona
+//
+// Para query com distancia de manhattan <= d, faca
+// nx = x+y, ny = x-y
+// Update em (nx, ny), query em ((nx-d, ny-d), (nx+d, ny+d))
+//
+// Se for de min/max, pode tirar os if's da 'query', e fazer
+// sempre as 4 operacoes. Fica mais rapido
+//
+// Complexidades:
+// build - O(n^2)
+// query - O(log^2(n))
+// update - O(log^2(n))
+
+
+struct seg2d{
+	ll n;
+	vector<v64> seg;
+
+	seg2d(ll n_, vector<v64>& init) : n(n_), seg(2 * (n+1), v64(2 * (n+1), 0)) {
+		forn(i,0,n) forn(j,0,n)
+			seg[i + n][j + n] = init[i][j];
+		for (ll x = 2*n; x; x--) for (ll y = 2*n; y; y--) {
+			if (x < n) seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
+			if (y < n) seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
+		}
+	}
+	
+	ll query(ll x1, ll y1, ll x2, ll y2) {
+		ll ret = 0, y3 = y1 + n, y4 = y2 + n;
+		for (x1 += n, x2 += n; x1 <= x2; ++x1 /= 2, --x2 /= 2)
+		for (y1 = y3, y2 = y4; y1 <= y2; ++y1 /= 2, --y2 /= 2) {
+			if (x1%2 == 1 && y1%2 == 1) ret += seg[x1][y1];
+			if (x1%2 == 1 && y2%2 == 0) ret += seg[x1][y2];
+			if (x2%2 == 0 && y1%2 == 1) ret += seg[x2][y1];
+			if (x2%2 == 0 && y2%2 == 0) ret += seg[x2][y2];
+		}
+		return ret;
+	}
+	
+	void update(ll x, ll y, ll val) {
+		ll y2 = y += n;
+		for (x += n; x; x /= 2, y = y2) {
+			if (x >= n) seg[x][y] = val;
+			else seg[x][y] = seg[2*x][y] + seg[2*x+1][y];
+			
+			while (y /= 2) seg[x][y] = seg[x][2*y] + seg[x][2*y+1];
+		}
+	}
+};
