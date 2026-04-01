011e79a3a2a58d85252dc6ae22b9ea618b21160b
MiguelABatista <miguel260503@gmail.com>
2026-04-01 15:37:53 -0300

    Update get_divisor

--- src/math/get_divisor.cpp
+++ src/math/get_divisor.cpp
@@ -1,24 +1,29 @@
 // Flattened Divisor Sieve (CSR format)
-// Precomputes all divisors up to MAX. 
+// Precomputes all divisors up to n. 
 // Faster than vector<v64> for cache reasons 
-// Time Complexity: O(MAX log MAX) | Space Complexity: O(MAX log MAX)
+// Time Complexity: O(n log n) | Space Complexity: O(n log n)
 
-void get_divisor() {
-    forn(i, 1, MAX)
-        for (ll j = i; j < MAX; j += i)
+tuple<v64, v64, v64> get_divisor(ll n) {
+    v64 head(n);
+    v64 tail(n);
+    v64 divs(n*(log(n)+1));
+
+    forn(i, 1, n)
+        for (ll j = i; j < n; j += i)
             head[j]++;
 
     ll curr = 0;
-    forn(i, 1, MAX) {
+    forn(i, 1, n) {
         ll cnt = head[i];
         head[i] = curr;
         tail[i] = curr;
         curr += cnt;
     }
 
-    forn(i, 1, MAX) 
-        for (ll j = i; j < MAX; j += i) 
-            divs[tail[j]++] = i;    
+    forn(i, 1, n) 
+        for (ll j = i; j < n; j += i) 
+            divs[tail[j]++] = i; 
+    return {head, tail, divs};   
 }
 // for d | x =
 // forn(k,head[x], tail[x]){
