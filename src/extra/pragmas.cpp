66a6b6b1c961dde265aec52050263a75424647d5
MiguelABatista <miguel260503@gmail.com>
2026-04-28 10:47:10 -0300

    Update pragma

--- src/extra/pragmas.cpp
+++ src/extra/pragmas.cpp
@@ -1,4 +1,11 @@
-// Pragmas
-#pragma GCC optimize("O3,unroll-loops,fast-math")
-#pragma GCC optimize("Ofast,fast-math,unroll-loops,inline")
-#pragma GCC optimize("Os")
+// Otimizacoes agressivas, pode deixar mais rapido ou mais devagar
+#pragma GCC optimize("Ofast")
+// Soft Ofast
+#pragma GCC optimize("O3")
+// Auto explicativo
+#pragma GCC optimize("unroll-loops")
+// Vetorizacao
+#pragma GCC target("avx2")
+// Para operacoes com bits
+#pragma GCC target("bmi,bmi2,popcnt,lzcnt")
+// tem que juntar os target na mesma linha
